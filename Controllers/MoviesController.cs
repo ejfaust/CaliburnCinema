cd384d9715af9af992df1d3bf5829a6d354a8c7a
Eric Faust <eric.faust@bellsouth.net>
2020-01-26 14:27:29 -0500

    Add and Update Movies Form

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -6,6 +6,7 @@ using System.Web.Mvc;
 using Caliburn.Models;
 using Caliburn.ViewModels;
 using System.Data.Entity;
+using System.Data.Entity.Validation;
 
 namespace Caliburn.Controllers
 {
@@ -29,6 +30,34 @@ namespace Caliburn.Controllers
             return View(movies);
         }
 
+        public ViewResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
         public ActionResult Details(int id)
         {
             var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
@@ -37,6 +66,28 @@ namespace Caliburn.Controllers
                 return HttpNotFound();
 
             return View(movie);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Movies");
         }
 
         // GET: Movie/Random
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -14,14 +14,20 @@ namespace Caliburn.Models
         [StringLength(255)]
         public string Name { get; set; }
 
-        [Required]
+        [Display(Name = "Genre")]
         public Genre Genre { get; set; }
+
+        //Required is placed here bc validation issues crop up with lacking Genre on Save().
+        //MVC does not see the difference from a database perspective, migration will not see the difference.
+        [Required] 
         public byte GenreId { get; set; }
 
         public DateTime DateAdded { get; set; }
 
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
 
+        [Display(Name = "Number in Stock")]
         public byte NumberInStock { get; set; }
     }//end class Movie
 }//end namespace Caliburn.Models
