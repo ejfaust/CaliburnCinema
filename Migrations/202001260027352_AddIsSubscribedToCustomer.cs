e56659843f6bf5472508ef6728fcafe4cbcd4d14
Eric Faust <eric.faust@bellsouth.net>
2020-01-26 08:14:09 -0500

    Up to working with LocalDb
    All work done prior to building Forms.

---
+++ Migrations/202001260027352_AddIsSubscribedToCustomer.cs
@@ -0,0 +1,18 @@
+﻿namespace Caliburn.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddIsSubscribedToCustomer : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "IsSubscriberToNewsletter", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "IsSubscriberToNewsletter");
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Caliburn.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        public bool IsSubscriberToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        public byte MembershipTypeId { get; set; } //EntityFramework recognizes blahblahblahId convention and treats it as a foreign key.
+        public DateTime? Birthdate { get; set; }
+    }
+}
